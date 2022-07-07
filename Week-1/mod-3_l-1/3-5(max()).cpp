51b3eb52a15f9dde3e43744f08c89d0471b8d080
nazmulmilon <milonsarker73@gmail.com>
2022-07-07 20:13:58 +0600

    mod-3 all c++ programs

--- Week-1/mod-3_l-1/3-5(max()).cpp
+++ Week-1/mod-3_l-1/3-5(max()).cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main()
 {
     int a=20, b=15, c=11, d=9;
-    int mn;
-    //mn = min(a,b);
-    //mn = min(a, min(b,c));
-    mn = min(a, min(b, min(c,d)));
-    cout<<mn<<endl;
+    int mx;
+    //mx = min(a,b);
+    //mx = min(a, min(b,c));
+    mx = max(a, min(b, min(c,d)));
+    cout<<mx<<endl;
 
     int ar[5]={5,2,3,1,0}, n=5;
 
-    int mn2=ar[0];
+    int mx2=ar[0];
     for(int i=0; i<n; i++)
     {
         //if(ar[i]<mn2){
             //mn2 = ar[i];
         //}//
-        mn2 = min(mn2, ar[i]);
+        mx2 = max(mx2, ar[i]);
     }
-    cout<<mn2<<endl;
+    cout<<mx2<<endl;
 }
