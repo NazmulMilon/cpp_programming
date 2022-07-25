7dc72302bf9df3399fbc1775529fa727823c35df
nazmulmilon <milonsarker73@gmail.com>
2022-07-25 20:31:28 +0600

    bank account create using oop concept

---
+++ Week-2/mod-7_lab-2/7-2(create_bank_account).cpp
@@ -0,0 +1,63 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class BankAccount
+{
+public:
+    string account_holder;
+    string address;
+    int age;
+    int account_number;
+protected:
+    int balance;
+private:
+    string password;
+public:
+    BankAccount(string account_holder, string address, int age, string password)
+    {
+        this->account_holder=account_holder;
+        this->address=address;
+        this->age=age;
+        this->password=password;
+        this->account_number=rand()%1000000000;
+        this->balance=0;
+        cout<<"Your Account no is: "<<this->account_number<<endl;
+    }
+
+    int show_balance(string password)
+    {
+        if(this->password==password)
+        {
+            return this->balance;
+        }
+        else{
+            return -1;
+        }
+    }
+};
+
+BankAccount* create_account()
+{
+    string account_holder, password, address;
+    int age;
+    cout<<"CREATE ACCOUNT"<<endl;
+    cin>>account_holder>>address>>age>>password;
+    BankAccount *myAccount = new BankAccount(account_holder, address, age,password);
+    return myAccount;
+}
+
+int main()
+{
+    //create_account();
+    BankAccount *myAccount = create_account();
+    if(myAccount->show_balance("abc") == -1)
+    {
+        cout<<"Password didn't match"<<endl;
+    }
+    else{
+        cout<<"Your balance is: "<<myAccount->show_balance("abc")<<endl;
+    }
+
+    return 0;
+}
